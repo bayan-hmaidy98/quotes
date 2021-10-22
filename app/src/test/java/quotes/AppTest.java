cae670798e40be619dc11a05fd9e945c48982962
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-10-22 23:09:03 +0300

    finish lab09

--- app/src/test/java/quotes/AppTest.java
+++ app/src/test/java/quotes/AppTest.java
@@ -6,10 +6,10 @@ package quotes;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Reader;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,5 +33,23 @@ class AppTest {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test void testApi() throws IOException {
+        try {
+            URL url = new URL("https://favqs.com/api/qotd");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+            InputStream inputStream = connection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader input = new BufferedReader(inputStreamReader);
+            String quoteFromApi = input.toString();
+            assertNotEquals(quoteFromApi, null);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
